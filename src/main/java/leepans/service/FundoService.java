f36ab650ecd3de179b216eaef8bda90101695fc7
Leandro <leandrotavares@unitins.br>
2026-04-02 12:16:33 -0300

    Add Service classes of Fundo & Tampa

---
+++ src/main/java/leepans/service/FundoService.java
@@ -0,0 +1,60 @@
+package leepans.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import leepans.dto.FundoRequestDTO;
+import leepans.model.Fundo;
+import leepans.repository.CorRepository;
+import leepans.repository.FundoRepository;
+import leepans.repository.MaterialRepository;
+
+import java.util.List;
+
+@ApplicationScoped
+public class FundoService implements GenericService<Fundo, FundoRequestDTO>{
+
+    @Inject
+    FundoRepository repository;
+
+    @Inject
+    CorRepository corRepository;
+
+    @Inject
+    MaterialRepository materialRepository;
+
+    @Override
+    public List<Fundo> findAll() {
+        return repository.findAll().list();
+    }
+
+    @Override
+    public Fundo findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Fundo create(Fundo entity) {
+        repository.persist(entity);
+        return entity;
+    }
+
+    @Override
+    public void update(Long id, FundoRequestDTO dto) {
+        Fundo fundo = findById(id);
+
+        if(fundo != null){
+            fundo.setCor(corRepository.findById(dto.idCor()));
+            fundo.setMaterial(materialRepository.findById(dto.idMaterial()));
+            fundo.setEspessura(dto.espessura());
+            fundo.setPeso(dto.peso());
+            fundo.setIsAntiaderente(dto.isAntiaderente());
+            repository.persist(fundo);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Fundo fundo = repository.findById(id);
+        if(fundo != null) repository.delete(fundo);
+    }
+}
---
+++ src/main/java/leepans/service/TampaService.java
@@ -0,0 +1,60 @@
+package leepans.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import leepans.dto.TampaRequestDTO;
+import leepans.model.Tampa;
+import leepans.repository.CorRepository;
+import leepans.repository.FundoRepository;
+import leepans.repository.MaterialRepository;
+import leepans.repository.TampaRepository;
+
+import java.util.List;
+
+@ApplicationScoped
+public class TampaService implements GenericService<Tampa, TampaRequestDTO> {
+
+    @Inject
+    TampaRepository repository;
+
+    @Inject
+    CorRepository corRepository;
+
+    @Inject
+    MaterialRepository materialRepository;
+
+    @Override
+    public List<Tampa> findAll() {
+        return repository.findAll().list();
+    }
+
+    @Override
+    public Tampa findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Tampa create(Tampa entity) {
+        repository.persist(entity);
+        return entity;
+    }
+
+    @Override
+    public void update(Long id, TampaRequestDTO dto) {
+        Tampa tampa = findById(id);
+
+        if(tampa!= null){
+            tampa.setCor(corRepository.findById(id));
+            tampa.setMaterial(materialRepository.findById(id));
+            tampa.setIsDePressao(dto.isDePressao());
+            tampa.setPeso(dto.peso());
+            repository.persist(tampa);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Tampa tampa = findById(id);
+        if(tampa != null) repository.delete(tampa);
+    }
+}

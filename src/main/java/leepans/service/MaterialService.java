e544105cb9a28235202440523f52cf346b8ae650
Leandro <leandrotavares@unitins.br>
2026-04-02 12:15:46 -0300

    Updated Service classes from Cor & Material & Sustentacao to use GenericService class

--- src/main/java/leepans/service/MaterialService.java
+++ src/main/java/leepans/service/MaterialService.java
@@ -2,14 +2,46 @@ package leepans.service;
 
 import java.util.List;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import leepans.dto.MaterialRequestDTO;
 import leepans.model.Material;
+import leepans.repository.MaterialRepository;
 
-public interface MaterialService {
+@ApplicationScoped
+public class MaterialService implements GenericService<Material, MaterialRequestDTO>{
 
-    List<Material> findAll();
-    Material findById(Long id);
-    Material create(Material material);
-    void update(Long id, MaterialRequestDTO dto);
-    void delete(Long id);
+    @Inject
+    MaterialRepository repository;
+
+    @Override
+    public List<Material> findAll() {
+        return repository.findAll().list();
+    }
+
+    @Override
+    public Material findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Material create(Material material) {
+        repository.persist(material);
+        return material;
+    }
+
+    @Override
+    public void update(Long id, MaterialRequestDTO dto) {
+        Material material = repository.findById(id);
+        if (material != null) {
+            material.setNome(dto.nome());
+            material.setQualidades(dto.qualidades());
+            repository.persist(material);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        repository.deleteById(id);
+    }
 }
--- src/main/java/leepans/service/SustentacaoService.java
+++ src/main/java/leepans/service/SustentacaoService.java
@@ -2,14 +2,66 @@ package leepans.service;
 
 import java.util.List;
 
+import org.hibernate.Hibernate;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import leepans.dto.SustentacaoRequestDTO;
 import leepans.model.Sustentacao;
+import leepans.repository.CorRepository;
+import leepans.repository.MaterialRepository;
+import leepans.repository.SustentacaoRepository;
 
-public interface SustentacaoService {
-    
-    List<Sustentacao> findAll();
-    Sustentacao findById(Long id);
-    Sustentacao create(Sustentacao sustentacao);
-    void update(Long id, SustentacaoRequestDTO dto);
-    void delete(Long id);
+@ApplicationScoped
+public class SustentacaoService implements GenericService<Sustentacao, SustentacaoRequestDTO>{
+
+    @Inject
+    SustentacaoRepository repository;
+
+    @Inject
+    CorRepository corRepository;
+
+    @Inject
+    MaterialRepository materialRepository;
+
+    @Override
+    public List<Sustentacao> findAll() {
+        return repository.findAll().list();
+    }
+
+    @Override
+    public Sustentacao findById(Long id) {
+        return repository.findById(id);
+    }
+
+    @Override
+    public Sustentacao create(Sustentacao sustentacao) {
+        repository.persist(sustentacao);
+        if(sustentacao.getMaterial() != null) {
+            Hibernate.initialize(sustentacao.getMaterial().getQualidades());
+        }
+        return sustentacao;
+    }
+
+    @Override
+    public void update(Long id, SustentacaoRequestDTO dto) {
+        Sustentacao sustentacao = repository.findById(id);
+        if (sustentacao != null) {
+            sustentacao.setPeso(dto.peso());
+            sustentacao.setMaterial(materialRepository.findById(dto.idMaterial()));
+            sustentacao.setCor(corRepository.findById(dto.idCor()));
+            sustentacao.setQuantidade(dto.quantidade());
+            sustentacao.setTamanhoEmCm(dto.tamanhoEmCm());
+            sustentacao.setTipoSustentacao(dto.tipoSustentacao());
+            repository.persist(sustentacao);
+        }
+    }
+
+    @Override
+    public void delete(Long id) {
+        Sustentacao sustentacao = repository.findById(id);
+        if (sustentacao != null) {
+            repository.delete(sustentacao);
+        }
+    }    
 }
